6364b09684f3794c44bd580d542794a8905cc7b8
CMNisal <cmnisal@gmail.com>
2016-08-01 17:26:12 +0530

    Initial Commit by Nisal 01/08/2016

---
+++ src/fnss/FnSS.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fnss;
+
+import fnss.gui.mainlogin;
+
+/**
+ *
+ * @author Nisal
+ */
+public class FnSS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mainlogin main = new mainlogin();
+        main.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
